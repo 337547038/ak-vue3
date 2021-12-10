608d99a8b3e526027b799373e040ed78108c4e34
3375477038 <3375477038@qq.com>
2021-12-10 14:44:35 +0800

    重构cascader

--- src/packages/cascader/index.vue
+++ src/packages/cascader/index.vue
@@ -2,7 +2,7 @@
 <template>
   <select-down
     v-bind="$props"
-    ref="selectDown"
+    ref="selectDownEl"
     :options="[]"
     :class="{[`${prefixCls}-cascader`]:true}"
     :model-value="showLabel"
@@ -80,8 +80,8 @@ export default defineComponent({
     downHeight: pType.number(0), // 显示下拉最大高度，超出显示滚动条
     options: pType.array<cityProps>(), // 参数非必要，来自于select
     // icon: pType.string('arrow'),
-    selectText: pType.array(['请选择省', '请选择市', '请选择区']),
-    listType: pType.string('city'), // 显示类型，这种模式只能单选multiple=false
+    // selectText: pType.array(['请选择省', '请选择市', '请选择区']),
+    // listType: pType.string('city'), // 显示类型，这种模式只能单选multiple=false
     lazy: pType.bool(),
     checkAny: pType.bool() // 选择任意一级选项
   },
@@ -92,7 +92,7 @@ export default defineComponent({
       checked: [], // 用来判断点击选中状态
       checkedMultiple: []// 用来判断点击选中状态，多选时需要使用，多选时临时点击
     })
-    const selectDown = ref()
+    const selectDownEl = ref()
     // 用于展示的值，格式化后显示于输入框的值
     const showLabel = computed(() => {
       // modelValue=[0,1,2] // 值模式
@@ -112,7 +112,6 @@ export default defineComponent({
           } else {
             // getLabelValue里有对downDataList赋值的方法，多选时不赋值
             const val = getOptionsItem(options, item, 'label')
-            // console.log('push:' + val)
             val && temp.push(val.label)
             options = val.children
           }
@@ -140,7 +139,7 @@ export default defineComponent({
     }
     // 下拉项点击
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const childrenClick = (obj, index, evt) => {
+    const childrenClick = (obj: cityProps, index: number, evt: any) => {
       if (!props.multiple && evt.target.className === 'checked') {
         return // 单选状态下点击选中的不处理
       }
@@ -149,6 +148,20 @@ export default defineComponent({
       // 追加当前项的子集
       if (obj.children) {
         state.downDataList.push(obj.children)
+      } else {
+        // 表示最后一级
+        if (!props.multiple) {
+          // 收起下拉，多选时点空白处收起
+          updateModelValue()
+        }
+      }
+      // 处理选中高亮状态
+      const val = obj.value === undefined ? obj.label : obj.value
+      if (props.multiple) {
+        state.checkedMultiple.splice(index, state.checkedMultiple.length - index)
+        state.checkedMultiple[index] = val
+      } else {
+        state.checked.splice(index, state.downDataList.length - index + 1, val)
       }
     }
     // 根据value值返回相应值
@@ -172,23 +185,6 @@ export default defineComponent({
       }
       return null
     }
-    // 根据value值返回对就的label，没有value时匹配label
-    /*const getLabelValue = (data: any, val: [string, number], setDown = true) => {
-      for (const key in data) {
-        const value = data[key].value
-        const hasValue = value === undefined ? data[key].label === val.toString() : value === val
-        if (hasValue) {
-          // const copyData = JSON.parse(JSON.stringify(data))
-          // setDown && state.downDataList.push(copyData)
-          return data[key].label
-        }
-        const labelV: string = getLabelValue(data[key].children, val, setDown)
-        if (labelV) {
-          return labelV
-        }
-      }
-      return null
-    }*/
     // 返回当前选中的栏目类名
     const getChecked = (li: cityProps, index: number) => {
       if (state.checked.length > 0) {
@@ -201,6 +197,7 @@ export default defineComponent({
               return h
             }
           }
+          return has(label, state.checkedMultiple, index)
         } else {
           return has(label, state.checked, index)
         }
@@ -215,6 +212,7 @@ export default defineComponent({
     // 展开下拉
     const toggleClick = () => {
       const length = props.modelValue.length
+      state.downDataList = [] // 先清空
       if (length === 0) {
         // 没有值时返回第一层
         state.downDataList = [props.options]
@@ -229,17 +227,22 @@ export default defineComponent({
           }
         }
       }
+      state.checkedMultiple = []
       if (length > 0) {
         // 处理下拉选中高亮
         state.checked = JSON.parse(JSON.stringify(props.modelValue))
       }
+    }
+    const updateModelValue = () => {
+      emit('update:modelValue', state.checked)
+      selectDownEl.value.slideUp()
     }
     onMounted(() => {
 
     })
     return {
       ...toRefs(state),
-      selectDown,
+      selectDownEl,
       clearClick,
       deleteClick,
       inputChange,
@@ -251,196 +254,5 @@ export default defineComponent({
       prefixCls
     }
   }
-  /*setup001(props, {emit}) {
-    const state = reactive({
-      activeLayer: 0, // 当前第几级
-      showLabel: [] // 用于展示的值，格式化后显示于输入框的值
-    })
-    // [index: string]: any;
-    const selectValue = ref<any>([])// 暂存的值，加工后的数组，同时保存了当前值所在数组的位置
-    const el = ref()
-    const init = () => {
-      let length = props.modelValue.length
-      // 有值
-      if (length > 0 && length <= 3) {
-        // 有值时要默认在最后一级，根据当前和取当前位置拼成数组
-        let data
-        for (let i = 0; i < length; i++) {
-          if (i === 0) {
-            data = props.data
-          } else if (i === 1) {
-            const index = selectValue.value[0].index
-            data = props.data[index].children
-          } else if (i === 2) {
-            const index = selectValue.value[0].index
-            const data1 = props.data[index].children
-            const index2 = selectValue.value[1].index
-            data = data1[index2].children
-          }
-          setDefaultValue(data, i)
-        }
-        state.activeLayer = length - 1
-        state.showValue = formatValue()
-      } else {
-        selectValue.value.push({
-          name: props.selectText[0],
-          index: 0
-        })
-      }
-    }
-    const children = computed(() => {
-      // 根据selectValue和当前的级数
-      let array: Array<any> = []
-      let data = props.data
-      if (state.activeLayer === 0) {
-
-      } else if (state.activeLayer === 1) {
-        // 根据索引直接找到当前的子级，省去一级级的遍历
-        const index = selectValue.value[0].index
-        data = props.data[index].children
-      } else if (state.activeLayer === 2) {
-        const index = selectValue.value[0].index
-        const data1 = props.data[index].children
-        const index2 = selectValue.value[1].index
-        data = data1[index2].children
-      }
-      for (let i in data) {
-        array.push({
-          name: data[i].name || data[i],
-          index: i,
-          hasChild: !!data[i].children
-        })
-      }
-      return array
-    })
-    const disabledOk = computed(() => {
-      return getFormDisabled(props.disabled)
-    })
-    const showHide = (e: MouseEvent) => {
-      if (el.value && el.value.contains(e.target)) {
-        if (disabledOk.value === true) {
-          return
-        }
-        state.show = !state.show
-      } else {
-        state.show = false
-      }
-    }
-    const setDefaultValue = (data: Array<any>, index: number) => {
-      for (let i in data) {
-        // 最后一个比较特别，是数组不是对象，没有name
-        if ((data[i].name || data[i]) === props.modelValue[index]) {
-          selectValue.value.push({
-            name: data[i].name || data[i],
-            index: i
-          })
-          break
-        }
-      }
-    }
-    const childrenClick = (item: any) => {
-      if (item.hasChild) {
-        if (state.activeLayer === 0) {
-          selectValue.value.splice(0, selectValue.value.length)// 清空
-          // 写入当前项
-          selectValue.value.push({
-            name: item.name,
-            index: item.index// 当前值在数据组中的位置，for时就可以直接找到当前项
-          })
-          // 写入第二项
-          selectValue.value.push({
-            name: props.selectText[1]
-          })
-        } else if (state.activeLayer === 1) {
-          // 将请选择修改为当前选择
-          selectValue.value[1] = {
-            name: item.name,
-            index: item.index
-          }
-          // 将第三级设为请选择
-          selectValue.value[2] = {name: props.selectText[2]}
-        }
-        state.activeLayer++// 跳到下一级
-      } else {
-        // 关闭下拉，将值给输入框，这里有可能是第二级
-        selectValue.value[state.activeLayer] = {
-          name: item.name,
-          index: item.index
-        }
-        // 如果只有二级时，这里清除下第三级，保证不出错
-        if (state.activeLayer === 1) {
-          selectValue.value.splice(2, 1)
-        }
-        state.showValue = formatValue()
-        state.show = false
-        state.showPlaceholder = false
-      }
-    }
-    const formatValue = (type?: boolean): any => {
-      // 将数组转为文本显示出来
-      let val = ''
-      let array = []
-      for (let i in selectValue.value) {
-        if (type) {
-          // 为真返回数组
-          array.push(selectValue.value[i].name)
-        } else {
-          val += selectValue.value[i].name
-        }
-      }
-      return type ? array : val
-    }
-    const stopPropagation = (e: MouseEvent) => {
-      e.stopPropagation()
-    }
-    const clearClick = () => {
-      state.showValue = ''
-      selectValue.value = []
-      state.activeLayer = 0
-      nextTick(() => {
-        selectValue.value.push({
-          name: props.selectText[0],
-          index: 0
-        })
-      })
-    }
-    watch(
-      () => state.showValue,
-      () => {
-        const value = formatValue(true)
-        // console.log(typeof value)
-        emit('update:modelValue', value)
-        emit('change', value)
-        controlChangeEvent(value)
-      }
-    )
-    onMounted(() => {
-      document.addEventListener('click', showHide)
-      init()
-      controlChangeEvent(props.modelValue, 'mounted')
-    })
-    onBeforeUnmount(() => {
-      document.addEventListener('click', showHide)
-    })
-    watch(() => props.modelValue, (v: any) => {
-      controlChangeEvent(v, 'mounted')
-    })
-    // formItem
-    const controlChange: any = inject(`${prefixCls}ControlChange`, '')
-    const controlChangeEvent = (val: any, type?: string) => {
-      controlChange && controlChange(val, type)
-    }
-    return {
-      prefixCls,
-      ...toRefs(state),
-      selectValue,
-      children,
-      el,
-      clearClick,
-      stopPropagation,
-      childrenClick,
-      disabledOk
-    }
-  }*/
 })
 </script>
