ceba63c84913a595911e5d23f6ecb21b98af3b73
337547038 <337547038@qq.com>
2022-03-22 21:01:16 +0800

    table添加tag显示样式

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,41 +1,31 @@
 <template>
-  <div style="height: 400px;overflow-y: auto" class="scroll-table">
-    <ak-table :data="tableData" ref="singleTable" fixedBottomScroll=".scroll-table">
-      <ak-column prop="date" label="Date" width="180" />
-      <ak-column prop="name" label="Name1" width="180" />
-      <ak-column prop="name" label="Name2" width="180" />
-      <ak-column prop="name" label="Name3" width="180" />
-      <ak-column prop="name" label="Name4" width="280" />
-      <ak-column prop="name" label="Name5" width="280" />
-      <ak-column prop="address" label="Address6" width="380"/>
-      <ak-column prop="address" label="Address7" width="280"/>
-      <ak-column prop="address" label="Address8" width="280" />
-    </ak-table>
-  </div>
+  <ak-table :data="tableData">
+    <ak-column label="日期" prop="date" />
+    <ak-column label="姓名" prop="name" />
+    <ak-column label="性别" prop="sex" :tag="{dict:{'男':'info','女':'danger'}}" />
+    <ak-column label="省份" prop="province" />
+    <ak-column label="城市" prop="city" />
+    <ak-column label="地址" prop="address" />
+    <ak-column label="邮编" prop="zip" />
+    <ak-column label="操作">
+      <template #default="scope">
+        <a @click="delClick(scope)">删除</a>
+      </template>
+    </ak-column>
+  </ak-table>
 </template>
-<script setup>
-import {ref} from 'vue'
-const tableData =ref([
-  {name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},{name: 'Tom',},
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-])
+<script>
+import tableData from '../packages/table/demoJs.json'
+
+export default {
+  setup() {
+    const delClick = item => {
+      alert(JSON.stringify(item))
+    }
+    return {
+      tableData,
+      delClick
+    }
+  }
+}
 </script>
