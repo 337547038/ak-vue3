03a5216f9c8484d80a45de52d6fb760cbd6bdad7
chenghaiwen <haiven_123@163.com>
2022-04-02 20:23:32 +0800

    fix(): 解决了eslint 和 pettier 冲突

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,81 +1,96 @@
-const { defineConfig } = require("eslint-define-config");
+const { defineConfig } = require('eslint-define-config')
 module.exports = defineConfig({
   root: true,
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es6: true
   },
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
-    sourceType: "module",
-    jsxPragma: "React",
+    sourceType: 'module',
+    jsxPragma: 'React',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   extends: [
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
     // 'plugin:jest/recommended'
   ],
-  plugins: ["vue", "prettier", "import", "@typescript-eslint"],
+  plugins: ['prettier', 'import', '@typescript-eslint'],
   rules: {
-    semi: ["error", "always"],
-    "vue/script-setup-uses-vars": "error",
-    "@typescript-eslint/ban-ts-ignore": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "vue/custom-event-name-casing": "off",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
+        printWidth: 80,
+        semi: false,
+        vueIndentScriptAndStyle: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        proseWrap: 'never',
+        htmlWhitespaceSensitivity: 'strict',
+        endOfLine: 'auto',
+        stylelintIntegration: true,
+        eslintIntegration: true,
+        trailingComma: 'none'
+      }
     ],
-    "no-unused-vars": [
-      "error",
+    'vue/script-setup-uses-vars': 'error',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
     ],
-    "space-before-function-paren": "off",
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
+    'space-before-function-paren': 'off',
 
-    "vue/attributes-order": "off",
-    "vue/one-component-per-file": "off",
-    "vue/html-closing-bracket-newline": "off",
-    "vue/max-attributes-per-line": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/attribute-hyphenation": "off",
-    "vue/require-default-prop": "off",
-    "vue/require-explicit-emits": "off",
-    "vue/html-self-closing": [
-      "error",
+    'vue/attributes-order': 'off',
+    'vue/one-component-per-file': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-explicit-emits': 'off',
+    'vue/html-self-closing': [
+      'error',
       {
         html: {
-          void: "always",
-          normal: "never",
-          component: "always",
+          void: 'always',
+          normal: 'never',
+          component: 'always'
         },
-        svg: "always",
-        math: "always",
-      },
+        svg: 'always',
+        math: 'always'
+      }
     ],
-    "vue/multi-word-component-names": "off",
-  },
-});
+    'vue/multi-word-component-names': 'off'
+  }
+})
