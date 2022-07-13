93f47d37f8818bc080a721170ce7b219190ea696
3375477038 <3375477038@qq.com>
2022-07-13 13:18:45 +0800

    update docs

---
+++ docs/assets/test.b8de7a10.js
@@ -0,0 +1,1 @@
+import{d as o,b as t,e as c,o as n,c as i,f as m}from"./index.f64dd1e4.js";const r={class:"image-demo"},d=o({setup(p){const e=t("./image/img2.png"),s=["./image/img1.png","./image/img2.png"];return(g,l)=>{const a=c("ak-image");return n(),i("div",r,[m(a,{src:e.value,class:"cls",style:{width:"100px",height:"100px"},previewSrcList:s},null,8,["src"])])}}});export{d as default};
