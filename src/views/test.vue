ffd9a451c53da99a58eecd9aae75bf39addb1b87
3375477038 <3375477038@qq.com>
2023-10-19 10:00:10 +0800

    文档

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,34 +1,4 @@
 <template>
-  <ak-select
-    placeholder="请选择"
-    :options="options"
-    v-model="value1"
-    :multiple="true"
-    :filterable="true"
-  />
+  <div></div>
 </template>
-<script>
-  import { ref } from 'vue'
-
-  export default {
-    setup() {
-      const options = ref([
-        { label: '选项1', value: '1' },
-        { label: '选项2', value: '2' },
-        { label: '选项3', value: '3' },
-        { label: '选项4', value: '4', disabled: true },
-        { label: '选项5', value: '5' },
-        { label: '6' },
-        { label: '选项7', value: '7' },
-        { label: '选项8', value: '8' },
-        { label: '选项9', value: '9' },
-        { label: '选项10', value: '10', class: 'red' }
-      ])
-      const value1 = ref(['4','1'])
-      return {
-        options,
-        value1
-      }
-    }
-  }
-</script>
+<script></script>
